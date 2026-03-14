996abc4c02acf1bf9df91ca3c6280ac5a0d30fe7
rapfa <rzavros@uclan.ac.uk>
2026-03-14 17:40:56 +0200

    Added the function where a future challenge is shown as locked and cannot be accessed.

--- docs/js/listChallenges.js
+++ docs/js/listChallenges.js
@@ -9,17 +9,33 @@ function listChallenges(){
 
 
             for (let i = 0; i < treasureHunts.length; i++) {
+
                 let li = document.createElement("li");
+
+                const hunt= treasureHunts[i];
+                const now= Date.now();
+                const startTime= hunt.startsOn;
+
+                let isLocked = now < startTime;
 
                 const params = new URLSearchParams({
                     uuid: treasureHunts[i].uuid,
                     name: treasureHunts[i].name
                 });
 
-                li.innerHTML = `<a href="./app.html?${params.toString()}" class="challenge-card-link">
+                if (isLocked) {
+
+                    li.innerHTML='<div class="challenge-card locked"> ' +
+                        '<span class="challenge-title">Future-treasure hunt 🔒</span>' +
+                        '<span class="challenge-description">This treasure hunt is not available yet</span>' +
+                        '<span class="challenge-start">Start: One thousand years</span></div>';
+                } else {
+
+                    li.innerHTML = `<a href="./app.html?${params.toString()}" class="challenge-card-link">
                     <span class="challenge-title">${treasureHunts[i].name}</span>
                     <span class="challenge-description">${treasureHunts[i].description}</span>
                 </a>`;
+                }
                 ul.appendChild(li);
             }
         })
